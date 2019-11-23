569b12123343c876e2aa7abcbab4b7809fdce7e7
Musa Talluzi <musa.talluzi@gmail.com>
2019-11-23 13:17:29 -0500

    Refactor TimetableController implementation details into TimetableService

--- optaplanner-integration/optaplanner-spring-boot-example/src/main/java/org/optaplanner/spring/boot/example/service/TimeTableController.java
+++ optaplanner-integration/optaplanner-spring-boot-example/src/main/java/org/optaplanner/spring/boot/example/service/TimeTableController.java
@@ -16,20 +16,12 @@
 
 package org.optaplanner.spring.boot.example.service;
 
-import java.time.DayOfWeek;
-import java.time.LocalTime;
-import java.util.ArrayList;
 import java.util.Comparator;
-import java.util.List;
-import java.util.concurrent.atomic.AtomicReference;
 
-import org.optaplanner.core.api.solver.SolverFuture;
-import org.optaplanner.core.api.solver.SolverManager;
 import org.optaplanner.spring.boot.example.domain.Lesson;
 import org.optaplanner.spring.boot.example.domain.Room;
 import org.optaplanner.spring.boot.example.domain.TimeTable;
 import org.optaplanner.spring.boot.example.domain.Timeslot;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -39,131 +31,36 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/timeTable")
 public class TimeTableController {
 
-    @Autowired
-    SolverManager<TimeTable> solverManager;
+    private final TimetableService timetableService;
 
-    private AtomicReference<TimeTable> timeTableReference;
-    private SolverFuture solverFuture = null;
-
-    private final RoomService roomService;
-    private final TimeslotService timeslotService;
-    private final LessonService lessonService;
-
-    public TimeTableController(RoomService roomService, TimeslotService timeslotService, LessonService lessonService) {
-        this.roomService = roomService;
-        this.timeslotService = timeslotService;
-        this.lessonService = lessonService;
-        timeTableReference = new AtomicReference<>(generateProblem());
+    public TimeTableController(TimetableService timetableService) {
+        this.timetableService = timetableService;
     }
 
     // To try, open http://localhost:8080/timeTable
     @RequestMapping()
     public TimeTable refreshTimeTable() {
-        return timeTableReference.get();
+        return timetableService.refreshTimeTable();
     }
 
     @PostMapping("/solve")
     public void solve() {
-        TimeTable timeTable = timeTableReference.get();
-        // TODO Race condition if room is added while solving, it disappears
-        solverFuture = solverManager.solve(timeTable, timeTableReference::set);
+        timetableService.solve();
     }
 
     @PostMapping("/addLesson")
     public void addLesson(@RequestBody Lesson lesson) {
-        timeTableReference.updateAndGet(timeTable -> {
-            long nextId = timeTable.getLessonList().stream()
-                    .map(Lesson::getId).max(Comparator.naturalOrder())
-                    .orElse(0L)
-                    + 1L;
-            timeTable.getLessonList().add(new Lesson(nextId,
-                    lesson.getSubject(), lesson.getTeacher(), lesson.getStudentGroup()));
-            return timeTable;
-        });
+        timetableService.addLesson(lesson);
     }
 
     @PostMapping("/addTimeslot")
     public void addTimeslot(@RequestBody Timeslot timeslot) {
-        timeTableReference.updateAndGet(timeTable -> {
-            long nextId = timeTable.getTimeslotList().stream()
-                    .map(Timeslot::getId).max(Comparator.naturalOrder())
-                    .orElse(0L)
-                    + 1L;
-            timeTable.getTimeslotList().add(new Timeslot(nextId,
-                    timeslot.getDayOfWeek(), timeslot.getStartTime(), timeslot.getEndTime()));
-            return timeTable;
-        });
+        timetableService.addTimeslot(timeslot);
     }
 
     // To try:  curl -d '{"name":"Room Z"}' -H "Content-Type: application/json" -X POST http://localhost:8080/timeTable/addRoom
     @PostMapping("/addRoom")
     public void addRoom(@RequestBody Room room) {
-        timeTableReference.updateAndGet(timeTable -> {
-            long nextId = timeTable.getRoomList().stream()
-                    .map(Room::getId).max(Comparator.naturalOrder())
-                    .orElse(0L)
-                    + 1L;
-            timeTable.getRoomList().add(new Room(nextId, room.getName()));
-            return timeTable;
-        });
+        timetableService.addRoom(room);
     }
-
-    public TimeTable generateProblem() {
-        List<Timeslot> timeslotList = new ArrayList<>(10);
-        long timeslotId = 1L;
-        timeslotList.add(new Timeslot(timeslotId++, DayOfWeek.MONDAY, LocalTime.of(8, 30), LocalTime.of(9, 30)));
-        timeslotList.add(new Timeslot(timeslotId++, DayOfWeek.MONDAY, LocalTime.of(9, 30), LocalTime.of(10, 30)));
-        timeslotList.add(new Timeslot(timeslotId++, DayOfWeek.MONDAY, LocalTime.of(10, 30), LocalTime.of(11, 30)));
-        timeslotList.add(new Timeslot(timeslotId++, DayOfWeek.MONDAY, LocalTime.of(13, 30), LocalTime.of(14, 30)));
-        timeslotList.add(new Timeslot(timeslotId++, DayOfWeek.MONDAY, LocalTime.of(14, 30), LocalTime.of(15, 30)));
-
-        timeslotList.add(new Timeslot(timeslotId++, DayOfWeek.TUESDAY, LocalTime.of(8, 30), LocalTime.of(9, 30)));
-        timeslotList.add(new Timeslot(timeslotId++, DayOfWeek.TUESDAY, LocalTime.of(9, 30), LocalTime.of(10, 30)));
-        timeslotList.add(new Timeslot(timeslotId++, DayOfWeek.TUESDAY, LocalTime.of(10, 30), LocalTime.of(11, 30)));
-        timeslotList.add(new Timeslot(timeslotId++, DayOfWeek.TUESDAY, LocalTime.of(13, 30), LocalTime.of(14, 30)));
-        timeslotList.add(new Timeslot(timeslotId++, DayOfWeek.TUESDAY, LocalTime.of(14, 30), LocalTime.of(15, 30)));
-        timeslotService.createTimeslots(timeslotList);
-
-        List<Room> roomList = new ArrayList<>(3);
-        long roomId = 1L;
-        roomList.add(new Room(roomId++, "Room A"));
-        roomList.add(new Room(roomId++, "Room B"));
-        roomList.add(new Room(roomId++, "Room C"));
-        roomService.createRooms(roomList);
-
-        List< Lesson > lessonList = new ArrayList<>();
-        long lessonId = 1L;
-        lessonList.add(new Lesson(lessonId++, "Math", "B. May", "9th grade"));
-        lessonList.add(new Lesson(lessonId++, "Math", "B. May", "9th grade"));
-        lessonList.add(new Lesson(lessonId++, "Physics", "M. Curie", "9th grade"));
-        lessonList.add(new Lesson(lessonId++, "Chemistry", "M. Curie", "9th grade"));
-        lessonList.add(new Lesson(lessonId++, "Geography", "M. Polo", "9th grade"));
-        lessonList.add(new Lesson(lessonId++, "History", "I. Jones", "9th grade"));
-        lessonList.add(new Lesson(lessonId++, "English", "I. Jones", "9th grade"));
-        lessonList.add(new Lesson(lessonId++, "English", "I. Jones", "9th grade"));
-        lessonList.add(new Lesson(lessonId++, "Spanish", "P. Cruz", "9th grade"));
-        lessonList.add(new Lesson(lessonId++, "Spanish", "P. Cruz", "9th grade"));
-
-        lessonList.add(new Lesson(lessonId++, "Math", "B. May", "10th grade"));
-        lessonList.add(new Lesson(lessonId++, "Math", "B. May", "10th grade"));
-        lessonList.add(new Lesson(lessonId++, "Math", "B. May", "10th grade"));
-        lessonList.add(new Lesson(lessonId++, "Physics", "M. Curie", "10th grade"));
-        lessonList.add(new Lesson(lessonId++, "Chemistry", "M. Curie", "10th grade"));
-        lessonList.add(new Lesson(lessonId++, "Geography", "M. Polo", "10th grade"));
-        lessonList.add(new Lesson(lessonId++, "History", "I. Jones", "10th grade"));
-        lessonList.add(new Lesson(lessonId++, "English", "P. Cruz", "10th grade"));
-        lessonList.add(new Lesson(lessonId++, "Spanish", "P. Cruz", "10th grade"));
-        lessonList.add(new Lesson(lessonId++, "French", "M. Curie", "10th grade"));
-
-        lessonList.get(4).setTimeslot(timeslotList.get(2));
-        lessonList.get(4).setRoom(roomList.get(0));
-        lessonList.get(5).setTimeslot(timeslotList.get(3));
-        lessonList.get(5).setRoom(roomList.get(1));
-        lessonList.get(6).setTimeslot(timeslotList.get(3));
-        lessonList.get(6).setRoom(roomList.get(1));
-        lessonService.createLessons(lessonList);
-
-        return new TimeTable(timeslotList, roomList, lessonList);
-    }
-
 }
